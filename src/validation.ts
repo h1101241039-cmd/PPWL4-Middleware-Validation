08f23f3bcc11333742229b39a3cc4135765a12a7
Florecita Wenny <h1101241039@student.untan.ac.id>
2026-03-03 18:28:27 +0700

    Praktikum3

--- src/validation.ts
+++ src/validation.ts
@@ -2,9 +2,22 @@ import { Elysia, t } from "elysia";
 import { openapi } from "@elysiajs/openapi";
 
 const app = new Elysia()
-  .use(openapi())
 
-  
+  .use(openapi())
+  .onRequest(({ request, set }) => {
+    console.log("📥", request.method, request.url);
+    console.log("🕒", new Date().toISOString());
+
+    // SHORT-CIRCUIT
+    if (request.headers.get("x-block") === "true") {
+      set.status = 403;
+      return {
+        message: "Blocked by middleware"
+      };
+    }
+  })
+
+  // PRAKTIKUM 1 - VALIDASI BODY
   .post(
     "/request",
     ({ body }) => {
@@ -19,7 +32,6 @@ const app = new Elysia()
         email: t.String({ format: "email" }),
         age: t.Number({ minimum: 18 })
       }),
-    
       response: t.Object({
         message: t.String(),
         data: t.Object({
@@ -31,7 +43,7 @@ const app = new Elysia()
     }
   )
 
-  
+  // PRAKTIKUM 2 - VALIDASI PARAMS & QUERY
   .get(
     "/products/:id",
     ({ params, query }) => {
@@ -52,7 +64,6 @@ const app = new Elysia()
           ])
         )
       }),
-
       response: t.Object({
         productId: t.Number(),
         sort: t.String()
@@ -60,7 +71,26 @@ const app = new Elysia()
     }
   )
 
+  // PRAKTIKUM 3 - VALIDASI RESPONSE
 
+  .get(
+    "/stats",
+    () => {
+      return {
+        total: 150,
+        active: 120
+      };
+    },
+    {
+      response: t.Object({
+        total: t.Number(),
+        active: t.Number()
+      })
+    }
+  )
+
+
+  
   .get(
     "/ping",
     () => {
@@ -77,6 +107,25 @@ const app = new Elysia()
     }
   )
 
+  
+  .get(
+    "/dashboard",
+    () => ({
+      message: "Welcome to Dashboard"
+    }),
+    {
+      beforeHandle({ headers, set }) {
+        if (!headers.authorization) {
+          set.status = 401;
+          return {
+            success: false,
+            message: "Unauthorized"
+          };
+        }
+      }
+    }
+  )
+
   .listen(3000);
 
-console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
+console.log("🦊 Server running at http://localhost:3000");
